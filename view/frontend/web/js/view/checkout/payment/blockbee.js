7a5e7db731f6e24eb18cec2a88a0a610e7429bd3
Ariano Fonseca Ângelo <geral@arianoangelo.com>
2025-03-07 18:11:29 +0000

    v1.1.1
    * Minor bugfixes

--- view/frontend/web/js/view/checkout/payment/blockbee.js
+++ view/frontend/web/js/view/checkout/payment/blockbee.js
@@ -1,11 +1,17 @@
 define([
-    'Magento_Checkout/js/view/payment/default'
-], function (Component) {
+    'Magento_Checkout/js/view/payment/default',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/model/payment/additional-validators',
+    'Magento_Checkout/js/action/place-order',
+    'Magento_Checkout/js/action/redirect-on-success',
+    'jquery',
+    'mage/translate'
+], function (Component, quote, additionalValidators, placeOrderAction, redirectOnSuccessAction, $, $t) {
     'use strict';
 
     return Component.extend({
         defaults: {
-            template: 'Magento_Checkout/js/view/payment/default'
+            template: 'Blockbee_Blockbee/checkout/payment/blockbee'
         },
         getInstructions: function () {
             return window.checkoutConfig.payment.blockbee.instructions;
@@ -17,15 +23,34 @@ define([
             };
         },
         placeOrder: function (data, event) {
+            var self = this;
+
             if (event) {
                 event.preventDefault();
             }
-            this.isPlaceOrderActionAllowed(false);
-            this._super();
+
+            if (this.validate() && additionalValidators.validate()) {
+                this.isPlaceOrderActionAllowed(false);
+
+                placeOrderAction(this.getData(), this.messageContainer)
+                    .done(function () {
+                        redirectOnSuccessAction.execute();
+                    })
+                    .fail(function (response) {
+                        self.isPlaceOrderActionAllowed(true);
+                        self.messageContainer.addErrorMessage({
+                            message: $t('An error occurred while processing your payment. Please try again.')
+                        });
+                    });
+
+                return true;
+            }
+
+            return false;
         },
         validate: function () {
-            var $form = jQuery('#co-payment-form');
-            return $form.valid && $form.valid();
+            var $form = $('#co-payment-form');
+            return $form.length ? $form.valid() : true;
         },
         afterPlaceOrder: function () {
             // The redirection will be handled automatically by Magento
